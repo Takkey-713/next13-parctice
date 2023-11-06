d3035a1c2effa559ee89d5fffa54a1c01ee3dfda
Takkey-713 <takiyu713@gmail.com>
2023-11-06 18:10:07 +0900

    add: next-13検証用リポジトリ反映

---
+++ app/api/notes/route.ts
@@ -0,0 +1,20 @@
+import { zUpsertNote } from "@/app/notes/type";
+import { prisma } from "@/globals/db";
+import { NextRequest, NextResponse } from "next/server";
+
+export const dynamic = 'force-dynamic';
+
+export async function GET() {
+  await new Promise(resolve => setTimeout(resolve, 3000));
+  const notes = await prisma.note.findMany();
+  return NextResponse.json(notes)
+}
+
+export async function POST(req: NextRequest) {
+  const data = await req.json();
+  const parcedData = zUpsertNote.parse(data);
+  const note = await prisma.note.create({
+    data: { title: parcedData.title, body: parcedData.body },
+  });
+  return new NextResponse(`${note.id}`, { status: 201 })
+}
---
+++ app/notes/page.tsx
@@ -0,0 +1,45 @@
+import ErrorBoundary from '@/components/ErrorBoundary';
+import FetchError from '@/components/FetchError';
+import Loading from '@/components/Loading';
+import { apiUrl } from "@/constants/api";
+import Link from 'next/link';
+import { Suspense } from 'react';
+import "server-only";
+import NoteList from './NoteList';
+import { zNotes } from "./type";
+
+export const revalidate = 0;
+
+export const metadata = {
+  title: "List Notes",
+}
+
+export default async function Page() {
+  const notes = await getNotes();
+  return (
+    <main className="mx-2 sm:mx-4 relative">
+      {/* @ts-expect-error Server Component */}
+      <Link href="/notes/new" className="absolute top-0 right-2 z-10 text-white bg-pink-500 hover:bg-pink-700 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center mr-2">
+        <svg aria-hidden="true" className="w-6 h-6" fill="currentColor" viewBox="4 4 8 8" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" clipRule="evenodd"></path></svg>
+        <span className="sr-only">New Note</span>
+      </Link>
+      <h2 className='mb-6 text-gray-400 text-xs'>List Notes</h2>
+      {/* @ts-expect-error Server Component */}
+      <ErrorBoundary fallback={<FetchError />}>
+        {/* @ts-expect-error Server Component */}
+        <Suspense fallback={<Loading />}>
+          {/* @ts-expect-error Server Component */}
+          <NoteList initialState={notes} />
+        </Suspense>
+      </ErrorBoundary>
+    </main>
+  )
+}
+
+const getNotes = async () => {
+  const res = await fetch(`${apiUrl}/notes`, { cache: 'no-store' });
+  const data = await res.json();
+  const notes = zNotes.parse(data);
+
+  return notes;
+};
